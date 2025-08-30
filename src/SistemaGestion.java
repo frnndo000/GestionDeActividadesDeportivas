8ffb558beb8bff0083b44502e426c608030924cd
Fernando <fernando.lopez.m01@mail.pucv.cl>
2025-08-30 15:54:48 -0400

    feat: Se implento la clase SistemaGestion que corresponde a la coleccion tipo A.

---
+++ src/SistemaGestion.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Fernando
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+        
+public class SistemaGestion {
+    private List<Cancha> canchas ;
+    
+    public SistemaGestion() {
+        this.canchas = new ArrayList<>() ;
+        
+        this.canchas.add(new Cancha(1, "Cancha 1")) ;
+        this.canchas.add(new Cancha(2, "Cancha 2")) ;
+    }
+    
+    public List<Cancha> getCanchas() { return canchas ; }
+}

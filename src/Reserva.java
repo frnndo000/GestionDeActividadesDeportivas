b6da2fdcac9b3bad89551edc289c62de2a268347
Your Name <you@example.com>
2025-08-29 23:45:09 -0400

    feat: Se creo la clase Reserva. La implementación de BloqueHorario se realizará en el próximo commit.

---
+++ src/Reserva.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Fernando
+ */
+
+import java.time.LocalDate;
+
+public class Reserva {
+    // Atributos
+    private int idReserva ;
+    private String rutSocio ;
+    private LocalDate fecha ;
+    private BloqueHorario bloque ;
+    
+    // Constructor
+    public Reserva(int idReserva, String rutSocio, LocalDate fecha, BloqueHorario bloque) {
+        this.idReserva = idReserva;
+        this.rutSocio = rutSocio;
+        this.fecha = fecha;
+        this.bloque = bloque;
+    }
+    
+    // Getters y Setters
+    public int getIdReserva() { return idReserva; }
+    public void setIdReserva(int idReserva) { this.idReserva = idReserva; }
+    public String getRutSocio() { return rutSocio; }
+    public void setRutSocio(String rutSocio) { this.rutSocio = rutSocio; }
+    public LocalDate getFecha() { return fecha; }
+    public void setFecha(LocalDate fecha) { this.fecha = fecha; }
+    public BloqueHorario getBloque() { return bloque; }
+    public void setBloque(BloqueHorario bloque) { this.bloque = bloque; }
+}

02ea6c8ffde39a3c38049b8239077ab59ad1afbc
Your Name <you@example.com>
2025-08-29 23:16:05 -0400

    feat: Se implemento la clase Socio.

--- src/gestiondecanchas/GestionDeCanchas.java
+++ src/gestiondecanchas/GestionDeCanchas.java
@@ -14,7 +14,7 @@ public class GestionDeCanchas {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("hola");
+        System.out.println("hola mundo");
     }
 }
 

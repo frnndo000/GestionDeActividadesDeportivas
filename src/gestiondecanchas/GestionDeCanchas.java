9a0e865d8db699492e71d3881f0a0b2d60a5f261
Your Name <you@example.com>
2025-08-29 23:12:51 -0400

    feat: Se implemento la clase Socio.

--- src/gestiondecanchas/GestionDeCanchas.java
+++ src/gestiondecanchas/GestionDeCanchas.java
@@ -14,7 +14,36 @@ public class GestionDeCanchas {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("hola");
     }
+}
+
+public class Socio {
+    // Atributos
+    private int rut ;
+    private String nombre ;
+    private int telefono ;
+    
+    // Constructores
+    public Socio(int rut, String nombre, int telefono) {
+        this.rut = rut ;
+        this.nombre = nombre ;
+        this.telefono = telefono ;   
+    }
+    
+    public Socio(int rut, String nombre) {
+        this.rut = rut ;
+        this.nombre = nombre ;
+        this.telefono = 0 ;   
+    }
+    
+    
+    // Getters y setters
+    public int getRut() { return rut; }
+    public void setRut(int rut) { this.rut = rut; }
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+    public int getTelefono() { return telefono; }
+    public void setTelefono(int telefono) { this.telefono = telefono; }
     
 }

1bcc0aaf1d740db19c248e09990fb071d9d8ab12
Your Name <you@example.com>
2025-08-30 20:04:47 -0400

    Estructuracion de la carpeta src

--- src/GestionDeCanchas.java
+++ src/GestionDeCanchas.java
@@ -2,8 +2,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package gestiondecanchas;
 
+
+import gestiondecanchas.SistemaGestion;
 import java.io.BufferedReader;
 import java.io.IO;
 import java.io.IOException;
